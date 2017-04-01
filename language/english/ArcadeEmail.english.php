38e7ad612ee1d94d2b4674d5e256c668b2b5b18e
Underdog-01 <github.underdog@gmail.com>
2017-04-01 03:48:59 -0400

    EOL conversion - Unix
    - incorrect EOL

--- language/english/ArcadeEmail.english.php
+++ language/english/ArcadeEmail.english.php
@@ -1,156 +1,156 @@
-<?php
-/**
- * SMF Arcade
- *
- * @package SMF Arcade
- * @version 2.5
- * @license http://web-develop.ca/index.php?page=arcade_license_BSD2 BSD 2
- */
-
-// SMF 2.1 behaviour
-// Highscore email notifications should be set to HTML
-$txt['notification_arcade_new_champion_own_subject'] = 'You are no longer champion of {GAMENAMESUB}';
-$txt['notification_arcade_new_champion_own_body'] = '<div>You are no longer champion of {GAMENAME},</div>
-<div style="padding-top: 5px;"> {champion.name} has beaten your score and is a new champion!</div>
-<div style="padding-top: 5px;">To reclaim this title, {play.the.game} and get a score better than {champion.score}.</div>
-<div style="padding-top: 15px;">You may opt to disable this notification from your profile settings.</div>
-<div style="padding-top: 15px;">In order to save your score you must login.</div>
-<div style="padding-top: 25px;">{REGARDS}</div>';
-
-$txt['notification_arcade_new_champion_any_subject'] = '{old_champion.name} is no longer champion of {GAMENAMESUB}';
-$txt['notification_arcade_new_champion_any_body'] = '<div>{old_champion.name} is no longer champion of {GAMENAME},</div>
-<div style="padding-top: 5px;">{champion.name} has beaten {old_champion.name}\'s score and is a new champion!</div>
-<div style="padding-top: 15px;">You may opt to disable this notification from your profile settings.</div>
-<div style="padding-top: 15px;">In order to save your score you must login.</div>
-<div style="padding-top: 25px;">{REGARDS}</div>';
-
-// Highscore PM notifications should be plain text only
-$txt['notification_arcade_new_champion_ownPM_subject'] = 'You are no longer champion of {GAMENAMESUB}';
-$txt['notification_arcade_new_champion_ownPM_body'] = 'You are no longer champion of {GAMENAME},
-{champion.name} has beaten your score and is a new champion!
-To reclaim this title, {play.the.game} and get a score better than {champion.score}.
-
-You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
-
-
-{REGARDS}';
-
-$txt['notification_arcade_new_champion_anyPM_subject'] = '{old_champion.name} is no longer champion of {GAMENAMESUB}';
-$txt['notification_arcade_new_champion_anyPM_body'] = '{old_champion.name} is no longer champion of {GAMENAME},
-{champion.name} has beaten {old_champion.name}\'s score and is a new champion!
-
-You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
-
-
-{REGARDS}';
-
-// Arena email notifications should be set to plain text
-$txt['notification_arcade_arena_invite_subject'] = 'You are invited to join a match';
-$txt['notification_arcade_arena_invite_body'] = 'You have been invited to join match "{MATCHNAME}" on Arcade Arena.
-To accept or decline this offer, visit match\'s page in url below:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}';
-
-$txt['notification_arcade_arena_new_round_subject'] = '{MATCHNAME}: New Round begins';
-$txt['notification_arcade_arena_new_round_body'] = 'New Round has begun on match "{MATCHNAME}".
-Visit following url to play:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}';
-
-$txt['notification_arcade_arena_match_end_subject'] = '{MATCHNAME}: Finished';
-$txt['notification_arcade_arena_match_end_body'] = 'Match "{MATCHNAME}" has been finished.
-Visit following url to see results:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}';
-
-// SMF 2.0 behaviour
-// Highscore email notifications should be set to HTML
-$txt['emails']['notification_arcade_new_champion_own'] = array(
-	'subject' => 'You are no longer champion of {GAMENAMESUB}',
-	'body' => '<div>You are no longer champion of {GAMENAME},</div>
-<div style="padding-top: 5px;"> {champion.name} has beaten your score and is a new champion!</div>
-<div style="padding-top: 5px;">To reclaim this title, {play.the.game} and get a score better than {champion.score}.</div>
-<div style="padding-top: 15px;">You may opt to disable this notification from: {ARCADE_SETTINGS_URL}</div>
-<div style="padding-top: 25px;">{REGARDS}</div>');
-
-$txt['emails']['notification_arcade_new_champion_any'] = array(
-	'subject' => '{old_champion.name} is no longer champion of {GAMENAMESUB}',
-	'body' => '<div>{old_champion.name} is no longer champion of {GAMENAME},</div>
-<div style="padding-top: 5px;">{champion.name} has beaten {old_champion.name}\'s score and is a new champion!</div>
-<div style="padding-top: 15px;">You may opt to disable this notification from: {ARCADE_SETTINGS_URL}</div>
-<div style="padding-top: 25px;">{REGARDS}</div>');
-
-// Highscore PM notifications should be plain text only
-$txt['emails']['notification_arcade_new_champion_ownPM'] = array(
-	'subject' => 'You are no longer champion of {GAMENAMESUB}',
-	'body' => 'You are no longer champion of {GAMENAME},
-{champion.name} has beaten your score and is a new champion!
-To reclaim this title, {play.the.game} and get a score better than {champion.score}.
-
-You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
-
-
-{REGARDS}');
-
-$txt['emails']['notification_arcade_new_champion_anyPM'] = array(
-	'subject' => '{old_champion.name} is no longer champion of {GAMENAMESUB}',
-	'body' => '{old_champion.name} is no longer champion of {GAMENAME},
-{champion.name} has beaten {old_champion.name}\'s score and is a new champion!
-
-You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
-
-
-{REGARDS}');
-
-// Arena email notifications should be set to plain text
-$txt['emails']['notification_arcade_arena_invite'] = array(
-	'subject' => 'You are invited to join a match',
-	'body' => 'You have been invited to join match "{MATCHNAME}" on Arcade Arena.
-To accept or decline this offer, visit match\'s page in url below:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}');
-
-$txt['emails']['notification_arcade_arena_new_round'] = array(
-	'subject' => '{MATCHNAME}: New Round begins',
-	'body' => 'New Round has begun on match "{MATCHNAME}".
-Visit following url to play:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}');
-
-$txt['emails']['notification_arcade_arena_match_end'] = array(
-	'subject' => '{MATCHNAME}: Finished',
-	'body' => 'Match "{MATCHNAME}" has been finished.
-Visit following url to see results:
-{MATCHURL}
-
-If you want to, You can disable this notification from
-{ARCADE_SETTINGS_URL}
-
-{REGARDS}');
-
-// General replacements
-$txt['arcade_pm_play_game'] = 'PLAY THE GAME';
-$txt['arcade_pm_join_match'] = 'PLAY THE MATCH';
-$txt['arcade_pm_profile'] = 'VIEW PROFILE';
-$txt['arcade_email_profile'] = 'You may opt to disable this notification from your profile settings.';
+<?php
+/**
+ * SMF Arcade
+ *
+ * @package SMF Arcade
+ * @version 2.5
+ * @license http://web-develop.ca/index.php?page=arcade_license_BSD2 BSD 2
+ */
+
+// SMF 2.1 behaviour
+// Highscore email notifications should be set to HTML
+$txt['notification_arcade_new_champion_own_subject'] = 'You are no longer champion of {GAMENAMESUB}';
+$txt['notification_arcade_new_champion_own_body'] = '<div>You are no longer champion of {GAMENAME},</div>
+<div style="padding-top: 5px;"> {champion.name} has beaten your score and is a new champion!</div>
+<div style="padding-top: 5px;">To reclaim this title, {play.the.game} and get a score better than {champion.score}.</div>
+<div style="padding-top: 15px;">You may opt to disable this notification from your profile settings.</div>
+<div style="padding-top: 15px;">In order to save your score you must login.</div>
+<div style="padding-top: 25px;">{REGARDS}</div>';
+
+$txt['notification_arcade_new_champion_any_subject'] = '{old_champion.name} is no longer champion of {GAMENAMESUB}';
+$txt['notification_arcade_new_champion_any_body'] = '<div>{old_champion.name} is no longer champion of {GAMENAME},</div>
+<div style="padding-top: 5px;">{champion.name} has beaten {old_champion.name}\'s score and is a new champion!</div>
+<div style="padding-top: 15px;">You may opt to disable this notification from your profile settings.</div>
+<div style="padding-top: 15px;">In order to save your score you must login.</div>
+<div style="padding-top: 25px;">{REGARDS}</div>';
+
+// Highscore PM notifications should be plain text only
+$txt['notification_arcade_new_champion_ownPM_subject'] = 'You are no longer champion of {GAMENAMESUB}';
+$txt['notification_arcade_new_champion_ownPM_body'] = 'You are no longer champion of {GAMENAME},
+{champion.name} has beaten your score and is a new champion!
+To reclaim this title, {play.the.game} and get a score better than {champion.score}.
+
+You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
+
+
+{REGARDS}';
+
+$txt['notification_arcade_new_champion_anyPM_subject'] = '{old_champion.name} is no longer champion of {GAMENAMESUB}';
+$txt['notification_arcade_new_champion_anyPM_body'] = '{old_champion.name} is no longer champion of {GAMENAME},
+{champion.name} has beaten {old_champion.name}\'s score and is a new champion!
+
+You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
+
+
+{REGARDS}';
+
+// Arena email notifications should be set to plain text
+$txt['notification_arcade_arena_invite_subject'] = 'You are invited to join a match';
+$txt['notification_arcade_arena_invite_body'] = 'You have been invited to join match "{MATCHNAME}" on Arcade Arena.
+To accept or decline this offer, visit match\'s page in url below:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}';
+
+$txt['notification_arcade_arena_new_round_subject'] = '{MATCHNAME}: New Round begins';
+$txt['notification_arcade_arena_new_round_body'] = 'New Round has begun on match "{MATCHNAME}".
+Visit following url to play:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}';
+
+$txt['notification_arcade_arena_match_end_subject'] = '{MATCHNAME}: Finished';
+$txt['notification_arcade_arena_match_end_body'] = 'Match "{MATCHNAME}" has been finished.
+Visit following url to see results:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}';
+
+// SMF 2.0 behaviour
+// Highscore email notifications should be set to HTML
+$txt['emails']['notification_arcade_new_champion_own'] = array(
+	'subject' => 'You are no longer champion of {GAMENAMESUB}',
+	'body' => '<div>You are no longer champion of {GAMENAME},</div>
+<div style="padding-top: 5px;"> {champion.name} has beaten your score and is a new champion!</div>
+<div style="padding-top: 5px;">To reclaim this title, {play.the.game} and get a score better than {champion.score}.</div>
+<div style="padding-top: 15px;">You may opt to disable this notification from: {ARCADE_SETTINGS_URL}</div>
+<div style="padding-top: 25px;">{REGARDS}</div>');
+
+$txt['emails']['notification_arcade_new_champion_any'] = array(
+	'subject' => '{old_champion.name} is no longer champion of {GAMENAMESUB}',
+	'body' => '<div>{old_champion.name} is no longer champion of {GAMENAME},</div>
+<div style="padding-top: 5px;">{champion.name} has beaten {old_champion.name}\'s score and is a new champion!</div>
+<div style="padding-top: 15px;">You may opt to disable this notification from: {ARCADE_SETTINGS_URL}</div>
+<div style="padding-top: 25px;">{REGARDS}</div>');
+
+// Highscore PM notifications should be plain text only
+$txt['emails']['notification_arcade_new_champion_ownPM'] = array(
+	'subject' => 'You are no longer champion of {GAMENAMESUB}',
+	'body' => 'You are no longer champion of {GAMENAME},
+{champion.name} has beaten your score and is a new champion!
+To reclaim this title, {play.the.game} and get a score better than {champion.score}.
+
+You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
+
+
+{REGARDS}');
+
+$txt['emails']['notification_arcade_new_champion_anyPM'] = array(
+	'subject' => '{old_champion.name} is no longer champion of {GAMENAMESUB}',
+	'body' => '{old_champion.name} is no longer champion of {GAMENAME},
+{champion.name} has beaten {old_champion.name}\'s score and is a new champion!
+
+You may opt to disable this notification from: {ARCADE_SETTINGS_URL}
+
+
+{REGARDS}');
+
+// Arena email notifications should be set to plain text
+$txt['emails']['notification_arcade_arena_invite'] = array(
+	'subject' => 'You are invited to join a match',
+	'body' => 'You have been invited to join match "{MATCHNAME}" on Arcade Arena.
+To accept or decline this offer, visit match\'s page in url below:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}');
+
+$txt['emails']['notification_arcade_arena_new_round'] = array(
+	'subject' => '{MATCHNAME}: New Round begins',
+	'body' => 'New Round has begun on match "{MATCHNAME}".
+Visit following url to play:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}');
+
+$txt['emails']['notification_arcade_arena_match_end'] = array(
+	'subject' => '{MATCHNAME}: Finished',
+	'body' => 'Match "{MATCHNAME}" has been finished.
+Visit following url to see results:
+{MATCHURL}
+
+If you want to, You can disable this notification from
+{ARCADE_SETTINGS_URL}
+
+{REGARDS}');
+
+// General replacements
+$txt['arcade_pm_play_game'] = 'PLAY THE GAME';
+$txt['arcade_pm_join_match'] = 'PLAY THE MATCH';
+$txt['arcade_pm_profile'] = 'VIEW PROFILE';
+$txt['arcade_email_profile'] = 'You may opt to disable this notification from your profile settings.';
 ?>
--- language/english/ArcadeSkinA.english-utf8.php
+++ language/english/ArcadeSkinA.english-utf8.php
@@ -1,82 +1,82 @@
-<?php
-/**
- * SMF Arcade
- *
- * @package SMF Arcade
- * @version 2.5
- * @license http://web-develop.ca/index.php?page=arcade_license_BSD2 BSD 2
- */
-$txt['skin_arena'] = 'Arena Information';
-$txt['your_score'] = 'Your Best ';
-$txt['champ'] = 'Champ Is %s';
-$txt['champ_score'] = 'With ';
-$txt['num_plays'] = 'Plays ';
-$txt['not_applicable'] = 'n/a';
-$txt['no_description'] = 'No Description Entered';
-$txt['no_new_champs'] = 'No New Champions';
-$txt['the_new_champs'] = '%s is the new champion of <a href="%s" title=" On %s ">%s</a><br />';
-$txt['alt_play'] = 'Play Game';
-$txt['alt_no_cats'] = ' Games Not Assigned A Category ';
-$txt['user_welcome'] = ' - Welcome ';
-$txt['arcade_game_cats'] = 'Categories';
-$txt['arcade_no_cats'] = 'Unassigned (%d)';
-$txt['arcade_no_category'] = 'Unassigned';
-$txt['arcade_all_games'] = 'All Games (%d)';
-$txt['arcade_new_games'] = 'New Games';
-$txt['arcade_popular'] = 'Popular Games';
-$txt['arcade_popular_none'] = 'None Played';
-$txt['arcade_champs'] = 'Champions';
-$txt['cat_champs'] = 'Champions Of %s Category';
-$txt['arcade_random_game'] = 'Random Game';
-$txt['arcade_game_search'] = 'Game Search';
-$txt['arcade_game_sort'] = 'Game Sort';
-$txt['arcade_search_go'] = 'Go';
-$txt['arcade_search_starts'] = 'Starts With';
-$txt['arcade_search_contains'] = 'Contains';
-$txt['arcade_search_ends'] = 'Ends With';
-$txt['arcade_daily'] = 'Daily Challenge';
-$txt['arcade_daily_none'] = 'No Scores Today';
-$txt['arcade_scores_none'] = 'No Scores Yet';
-$txt['arcade_members_only'] = ' Members Only';
-$txt['arcade_file_non_read'] = 'Unable to read tar file';
-$txt['top_spots'] = 'Top Spots';
-$txt['latest_games'] = 'Latest Games';
-$txt['welcome'] = 'Welcome';
-$txt['latest_champs'] = 'Latest Champs';
-$txt['win'] = 'Wins';
-$txt['champ_list_wins'] = 'Champ List (By Wins)';
-$txt['champ_list_scores'] = 'Champ List (By Scores)';
-$txt['champ_score'] = 'Score';
-$txt['champ_scoring'] = 'Scoring ';
-$txt['latest_scores'] = 'Latest Scores';
-$txt['arcade_guest'] = 'Guest';
-$txt['most_played'] = 'Most Played';
-$txt['todays_scores'] = 'Challenge Scores';
-$txt['arcade_leaderboard'] = 'Leaderboard';
-$txt['no_display'] = 'Nothing To Display';
-$txt['close_window'] = 'Close Window';
-$txt['skin_when'] = 'Score recorded %s';
-$txt['skin_new_champ'] = 'New Champion Of';
-$txt['arcade_defcat'] = 'All Categories';
-
-//select
-$txt['arcade_sort_by'] = 'Sort by...';
-$txt['arcade_age'] = 'Latest';
-$txt['arcade_a2z'] = 'A to Z';
-$txt['arcade_z2a'] = 'Z to A';
-$txt['arcade_plays'] = 'Most Popular';
-$txt['arcade_playsl'] = 'Least Popular';
-$txt['arcade_champion'] = 'Champions';
-$txt['arcade_latest_champions'] = 'Latest Champions';
-$txt['arcade_myscore'] = 'Your Score';
-$txt['arcade_rating'] = 'Rating';
-$txt['arcade_favs'] = 'Favorites';
-$txt['arcade_all'] = 'All';
-
-//Arcade Profile
-$txt['arcade_positional'] = 'Top Ten Positional Wins';
-$txt['arcade_1st'] = '1st Place';
-$txt['arcade_2nd'] = '2nd Place';
-$txt['arcade_3rd'] = '3rd Place';
-$txt['arcade_none'] = 'None Here';
+<?php
+/**
+ * SMF Arcade
+ *
+ * @package SMF Arcade
+ * @version 2.5
+ * @license http://web-develop.ca/index.php?page=arcade_license_BSD2 BSD 2
+ */
+$txt['skin_arena'] = 'Arena Information';
+$txt['your_score'] = 'Your Best ';
+$txt['champ'] = 'Champ Is %s';
+$txt['champ_score'] = 'With ';
+$txt['num_plays'] = 'Plays ';
+$txt['not_applicable'] = 'n/a';
+$txt['no_description'] = 'No Description Entered';
+$txt['no_new_champs'] = 'No New Champions';
+$txt['the_new_champs'] = '%s is the new champion of <a href="%s" title=" On %s ">%s</a><br />';
+$txt['alt_play'] = 'Play Game';
+$txt['alt_no_cats'] = ' Games Not Assigned A Category ';
+$txt['user_welcome'] = ' - Welcome ';
+$txt['arcade_game_cats'] = 'Categories';
+$txt['arcade_no_cats'] = 'Unassigned (%d)';
+$txt['arcade_no_category'] = 'Unassigned';
+$txt['arcade_all_games'] = 'All Games (%d)';
+$txt['arcade_new_games'] = 'New Games';
+$txt['arcade_popular'] = 'Popular Games';
+$txt['arcade_popular_none'] = 'None Played';
+$txt['arcade_champs'] = 'Champions';
+$txt['cat_champs'] = 'Champions Of %s Category';
+$txt['arcade_random_game'] = 'Random Game';
+$txt['arcade_game_search'] = 'Game Search';
+$txt['arcade_game_sort'] = 'Game Sort';
+$txt['arcade_search_go'] = 'Go';
+$txt['arcade_search_starts'] = 'Starts With';
+$txt['arcade_search_contains'] = 'Contains';
+$txt['arcade_search_ends'] = 'Ends With';
+$txt['arcade_daily'] = 'Daily Challenge';
+$txt['arcade_daily_none'] = 'No Scores Today';
+$txt['arcade_scores_none'] = 'No Scores Yet';
+$txt['arcade_members_only'] = ' Members Only';
+$txt['arcade_file_non_read'] = 'Unable to read tar file';
+$txt['top_spots'] = 'Top Spots';
+$txt['latest_games'] = 'Latest Games';
+$txt['welcome'] = 'Welcome';
+$txt['latest_champs'] = 'Latest Champs';
+$txt['win'] = 'Wins';
+$txt['champ_list_wins'] = 'Champ List (By Wins)';
+$txt['champ_list_scores'] = 'Champ List (By Scores)';
+$txt['champ_score'] = 'Score';
+$txt['champ_scoring'] = 'Scoring ';
+$txt['latest_scores'] = 'Latest Scores';
+$txt['arcade_guest'] = 'Guest';
+$txt['most_played'] = 'Most Played';
+$txt['todays_scores'] = 'Challenge Scores';
+$txt['arcade_leaderboard'] = 'Leaderboard';
+$txt['no_display'] = 'Nothing To Display';
+$txt['close_window'] = 'Close Window';
+$txt['skin_when'] = 'Score recorded %s';
+$txt['skin_new_champ'] = 'New Champion Of';
+$txt['arcade_defcat'] = 'All Categories';
+
+//select
+$txt['arcade_sort_by'] = 'Sort by...';
+$txt['arcade_age'] = 'Latest';
+$txt['arcade_a2z'] = 'A to Z';
+$txt['arcade_z2a'] = 'Z to A';
+$txt['arcade_plays'] = 'Most Popular';
+$txt['arcade_playsl'] = 'Least Popular';
+$txt['arcade_champion'] = 'Champions';
+$txt['arcade_latest_champions'] = 'Latest Champions';
+$txt['arcade_myscore'] = 'Your Score';
+$txt['arcade_rating'] = 'Rating';
+$txt['arcade_favs'] = 'Favorites';
+$txt['arcade_all'] = 'All';
+
+//Arcade Profile
+$txt['arcade_positional'] = 'Top Ten Positional Wins';
+$txt['arcade_1st'] = '1st Place';
+$txt['arcade_2nd'] = '2nd Place';
+$txt['arcade_3rd'] = '3rd Place';
+$txt['arcade_none'] = 'None Here';
 ?>
